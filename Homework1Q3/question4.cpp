73d433acffffab039350e4e8ac4546e284b6d274
haywoodjustin <49731705+haywoodjustin@users.noreply.github.com>
2022-03-03 15:16:59 -0500

    Kevin's Updates

--- Homework1Q3/question4.cpp
+++ Homework1Q3/question4.cpp
@@ -4,28 +4,76 @@ using namespace std;
 
 struct adjNode {
 	int val;
-	adjNode* next; 
+	adjNode* next = NULL; 
 };
 
 struct edge {
 	int start, end; 
 };
 
-class graph{
-	adjNode* getAdjListNode(int value, adjNode* head) {
-		adjNode* newNode = new adjNode; 
-		newNode->val = value;
-		newNode->next = head; 
-
-		return newNode; 
-	}
+class Graph {
+	int numNodes;
 public:
-
+	adjNode* heads[numNodes];
+	Graph(int nodeList) {
+		numNodes = nodeList[0]; // list[0] is the number of nodes in the graph.
+		int i = 1;
+		int j = 2;
+		while (nodeList[i] >= 0 && nodeList[j] >= 0) {
+			adjNode* edge = new adjNode;
+			edge->val = nodeList[j];
+			if (heads[i] == NULL) {
+				heads[i] = edge;
+				continue;
+			}
+			adjNode* endNode = heads[i];
+			while (endNode->next != NULL) {
+				endNode = endNode->next;
+			}
+			endNode->next = edge;
+			j = j + 2;
+			i = i + 2;
+		}
+	}
+	string edgeAddition(int from, int to) {
+		if (from > numNodes || to > numNodes) {
+			return "Invalid vertex given."; // Can't add an edge to a vertex that doesn't exist.
+		}
+		adjNode* edge = new adjNode;
+		edge->val = to;
+		if (heads[from] == NULL) {
+			heads[from] = edge;
+		}
+		else {
+			adjNode* endNode = heads[i];
+			while (endNode->next != NULL) {
+				endNode = endNode->next;
+			}
+			endNode->next = edge;
+		}
+		return "Edge added succesfully.";
+	}
+	string edgeDeletion(int from, int to){
+		if (from > numNodes || to > numNodes) {
+			return "Invalid vertex given."; // Can't delete an edge to a vertex that doesn't exist.
+		}
+		if (heads[from] == NULL) {
+			// There are no edges associated with the given from vertex.
+			return "Given from vertex does not have any associated edges."; 
+		}
+		adjNode* currNode = heads[i];
+		while (currNode->next != NULL) {
+			if (currNode)
+			currNode = currNode->next;
+		}
+		return "Edge deleted succesfully";
+	}
 };
  
 int main() {
 
-	edge edges[3] = { {1,2}, {0,3}, {4,3} }; 
+	int graphList = { 5,0,1,1,4,2,3,1,3,3,4,-1 };
+	Graph graph = new Graph(graphList);
 
 	return 0; // Hi Justin <3
-}
+};
